b841a6ca9736c257aab1270369e279de11ae65cd
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-21 12:55:48 +0530

    feat: zoneless change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { provideIonicAngular } from "@ionic/angular/standalone";
 
-import { routes } from './app.routes';
-import { provideIonicAngular } from '@ionic/angular/standalone';
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideIonicAngular({})]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideIonicAngular({}),
+  ],
 };
