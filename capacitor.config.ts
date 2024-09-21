4d622129925f307948370a82de4ef88d9b168b88
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-21 13:37:05 +0530

    fix: capacitor web dir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ng.ionic.material',
   appName: 'Ng IonMat',
-  webDir: 'www'
+  webDir: 'dist/mobile/browser'
 };
 
 export default config;
