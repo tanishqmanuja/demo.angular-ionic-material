7aea1926d08ac6bd127670a27560cb97fe3e914f
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-21 13:30:33 +0530

    feat: add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ng.ionic.material',
+  appName: 'Ng IonMat',
+  webDir: 'www'
+};
+
+export default config;
