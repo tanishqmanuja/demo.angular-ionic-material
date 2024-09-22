0b4e1e5ea81fedc81c28b36f18431fa8037b661b
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-22 18:56:08 +0530

    fix: splash flashing on app start

--- android/app/src/main/java/ng/ionic/material/MainActivity.java
+++ android/app/src/main/java/ng/ionic/material/MainActivity.java
@@ -1,5 +1,21 @@
 package ng.ionic.material;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int clr = getResources().getColor(R.color.colorSplashBackground);
+
+        WebView webview = this.getBridge().getWebView();
+        webview.setBackgroundColor(clr);
+
+        getWindow().setStatusBarColor(clr);
+        getWindow().setNavigationBarColor(clr);
+    }
+}
