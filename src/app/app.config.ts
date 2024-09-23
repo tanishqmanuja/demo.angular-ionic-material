3542cd594c8acf5a2e60bd736ca42311b29509f1
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-23 18:18:04 +0530

    feat: add styles for app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,18 @@ import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
 } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideIonicAngular } from "@ionic/angular/standalone";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import {
+  PreloadAllModules,
+  provideRouter,
+  RouteReuseStrategy,
+  withPreloading,
+  withViewTransitions,
+} from "@angular/router";
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from "@ionic/angular/standalone";
 import { Capacitor } from "@capacitor/core";
 
 import { SystemColors } from "@tqman/capacitor-system-colors";
@@ -13,9 +23,6 @@ import { provideTheme } from "./shared/providers/theme.provider";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes),
-    provideIonicAngular({}),
     provideTheme({
       color:
         Capacitor.getPlatform() === "android" &&
@@ -23,5 +30,14 @@ export const appConfig: ApplicationConfig = {
           ({ color }) => color,
         ),
     }),
+    provideIonicAngular(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideRouter(
+      routes,
+      withViewTransitions({ skipInitialTransition: true }),
+      withPreloading(PreloadAllModules),
+    ),
+    provideAnimationsAsync(),
+    provideExperimentalZonelessChangeDetection(),
   ],
 };
